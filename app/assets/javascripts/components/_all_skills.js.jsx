cbfe92c12380148c094c03c63c3ce5bf7f775fe0
MB Burch <marybethburch@gmail.com>
2015-12-18 07:51:46 -0700

    add ability to delete skill

--- app/assets/javascripts/components/_all_skills.js.jsx
+++ app/assets/javascripts/components/_all_skills.js.jsx
@@ -1,19 +1,17 @@
 var AllSkills = React.createClass({
-  getInitialState() {
-    return { skills: [] }
-  },
 
-  componentDidMount() {
-    $.getJSON('/api/v1/skills.json', (response) => { this.setState({ skills: response }) });
+  handleDelete(id) {
+    this.props.handleDelete(id);
   },
 
   render() {
-    var skills = this.state.skills.map((skill, index) => {
+    var skills = this.props.skills.map((skill, index) => {
       return (
         <div key={index}>
           <h3>{skill.name}</h3>
           <p><strong>Level:</strong> {skill.level}</p>
           <p>{skill.details}</p>
+          <button onClick={this.handleDelete.bind(this, skill.id)}>Delete</button>
         </div>
       )
     });
--- app/assets/javascripts/components/_body.js.jsx
+++ app/assets/javascripts/components/_body.js.jsx
@@ -1,9 +1,40 @@
 var Body = React.createClass({
+
+  getInitialState() {
+    return { skills: [] }
+  },
+
+  componentDidMount() {
+    $.getJSON('/api/v1/skills.json', (response) => { this.setState({ skills: response }) });
+  },
+
+  handleDelete(id) {
+    $.ajax({
+      url: `/api/v1/skills/${id}`,
+      type: 'DELETE',
+      success: () => {
+        this.removeIdeaFromDOM(id);
+      }
+    });
+  },
+
+  removeIdeaFromDOM(id) {
+    let newSkills = this.state.skills.filter((skill) => {
+      return skill.id !=id;
+    });
+    this.setState({ skills :newSkills });
+  },
+
+  handleSubmit(skill) {
+    let newState = this.state.skills.concast(skill);
+    this.setState({ skills: newState })
+  },
+
   render() {
     return (
       <div>
-        <NewSkill />
-        <AllSkills />
+        <NewSkill handleSubmit={this.handleSubmit} />
+        <AllSkills skills={this.state.skills} handleDelete={this.handleDelete} />
       </div>
     )
   }
