2c3a1cfa1b6760a59f3cef6ecef41faae6b5fe92
MB Burch <marybethburch@gmail.com>
2015-12-20 11:55:20 -0700

    prepare assets for heroku troubleshooting

---
+++ public/assets/components/_all_skills-4e5a4750eda9c40feea9df2d8b42d804c444574e78978d9e7a548e1c0a03c54d.jsx
@@ -0,0 +1,32 @@
+var AllSkills = React.createClass({
+
+  handleDelete(id) {
+    this.props.handleDelete(id);
+  },
+
+  handleEdit() {
+    console.log('you are in edit!');
+  },
+
+  onUpdate(skill) {
+    this.props.onUpdate(skill);
+  },
+
+  render() {
+    let skills = this.props.skills.map((skill, index) => {
+      return (
+        <div key={index}>
+          <Skill skill={skill}
+                 handleDelete={this.handleDelete.bind(this, skill.id)}
+                 handleUpdate={this.onUpdate}/>
+        </div>
+      )
+    });
+
+    return(
+      <div>
+        {skills}
+      </div>
+    )
+  }
+});
---
+++ public/assets/components/_body-79f9292a0e49119acd2d844db32036349cd0f422b913bf4f14d789d89b77aedd.jsx
@@ -0,0 +1,62 @@
+var Body = React.createClass({
+
+  getInitialState() {
+    return { skills: [] }
+  },
+
+  componentDidMount() {
+    $.getJSON('/api/v1/skills.json', (response) => { this.setState({ skills: response }) });
+  },
+
+  handleDelete(id) {
+    $.ajax({
+      url: `/api/v1/skills/${id}`,
+      type: 'DELETE',
+      success: () => {
+        this.removeIdeaFromDOM(id);
+      }
+    });
+  },
+
+  removeIdeaFromDOM(id) {
+    let newSkills = this.state.skills.filter((skill) => {
+      return skill.id !=id;
+    });
+    this.setState({ skills :newSkills });
+  },
+
+  handleSubmit(skill) {
+    let newState = this.state.skills.concat(skill);
+    this.setState({ skills: newState })
+  },
+
+  handleUpdate(skill) {
+    $.ajax({
+      url: `/api/v1/skills/${skill.id}`,
+      type: 'PUT',
+      data: { skill: skill },
+      success: (skill) => {
+        console.log('you did it');
+        this.updateSkills(skill);
+      }
+    });
+  },
+
+  updateSkills(skill) {
+    let skills = this.state.skills.filter((s) => { return s.id != skill.id });
+    skills.push(skill);
+
+    this.setState({ skills: skills });
+  },
+
+  render() {
+    return (
+      <div>
+        <NewSkill handleSubmit={this.handleSubmit} />
+        <AllSkills skills={this.state.skills}
+                   handleDelete={this.handleDelete}
+                   onUpdate={this.handleUpdate} />
+      </div>
+    )
+  }
+});
